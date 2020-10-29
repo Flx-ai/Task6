2997db39ed149864991bffd114c9f5305d816022
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-10-29 16:52:35 +0300

    Task6

--- src/vsu/cs/ru/num9/Main.java
+++ src/vsu/cs/ru/num9/Main.java
@@ -11,16 +11,24 @@ public class Main {
         double n = readValue("n");
 
         double sumOfSummandsThisSequence = calcSumOfSummandsThisSequence(x,n);
-        printResult("Сумма n слагаемых заданной последовательности = ", sumOfSummandsThisSequence);
+        printResult("Sum of n summands of this sequence = ", sumOfSummandsThisSequence);
+        System.out.println();
 
-        double sumOfSummandsThisSequenceWithEpsilon = calcSumOfSummandsThisSequenceWithEpsilon(x, epsilon);
-        printResult("Сумма слагаемых, которые по абсолютной величине > epsilon = ", sumOfSummandsThisSequenceWithEpsilon);
+        double[] arrayOfValues;
+        arrayOfValues = calcSumOfSummandsThisSequenceGreaterThanEpsilon(x, epsilon);
+        double numberOfSummandsGreaterThanEpsilon = arrayOfValues[0];
+        double sumOfSummandsThisSequenceGreaterThanEpsilon = arrayOfValues[1];
+        printResult("Number of summands with a value greater than epsilon = ", numberOfSummandsGreaterThanEpsilon,
+                        "Sum of summands with a value greater than epsilon = ", sumOfSummandsThisSequenceGreaterThanEpsilon);
 
-        double sumOfSummandsThisSequenceWithEpsilonDividedBy10 = calcSumOfSummandsThisSequenceWithEpsilon(x, epsilon/10);
-        printResult("Сумма слагаемых, которые по абсолютной величине > epsilon / 10 = ", sumOfSummandsThisSequenceWithEpsilonDividedBy10);
+        arrayOfValues = calcSumOfSummandsThisSequenceGreaterThanEpsilon(x, epsilon / 10);
+        double numberOfSummandsGreaterThanEpsilonDividedBy10 = arrayOfValues[0];
+        double sumOfSummandsThisSequenceGreaterThanEpsilonDividedBy10 = arrayOfValues[1];
+        printResult("Number of summands with a value greater than epsilon / 10 = ", numberOfSummandsGreaterThanEpsilonDividedBy10,
+                "Sum of summands with a value greater than epsilon / 10 = ", sumOfSummandsThisSequenceGreaterThanEpsilonDividedBy10);
 
         double valueOfFunctionUsingMath = calcValueOfFunctionUsingMath(x);
-        printResult("Значение заданной функции с помощью метода Math =  ", valueOfFunctionUsingMath);
+        printResult("Value of function using the Math = ", valueOfFunctionUsingMath);
     }
 
     private static double readValue(String name) {
@@ -51,26 +59,35 @@ public class Main {
         return sum;
     }
 
-    private static double calcSumOfSummandsThisSequenceWithEpsilon(double x, double epsilon) {
-        double summand = 0;
-        double nextSummand = 1;
-        double sum = 0;
+    private static double[] calcSumOfSummandsThisSequenceGreaterThanEpsilon(double x, double epsilon) {
+        double summand = 1;
+        double sumOfSummandsThisSequenceGreaterThanEpsilon = 0;
         double i = 0;
+        int numberOfSummandsGreaterThanEpsilon = 0;
 
-        while (Math.abs(summand - nextSummand) > epsilon) {
-            summand = nextSummand;
-            sum += summand;
+        while (Math.abs(summand) > epsilon) {
+            sumOfSummandsThisSequenceGreaterThanEpsilon += summand;
             i += 2;
-            nextSummand *= (i - 1) / i * pow(x, 2);
+            numberOfSummandsGreaterThanEpsilon++;
+            summand *= (i - 1) / i * pow(x, 2);
         }
-        return sum;
+
+        double[] arrayOfValues = new double[2];
+            arrayOfValues[0] = numberOfSummandsGreaterThanEpsilon;
+            arrayOfValues[1] = sumOfSummandsThisSequenceGreaterThanEpsilon;
+        return arrayOfValues;
     }
 
     private static double calcValueOfFunctionUsingMath(double x) {
         return 1 / Math.sqrt(1 - Math.pow(x,2));
     }
 
-    private static void printResult(String str, double result) {
-        System.out.println(str + result);
+    private static void printResult(String firstPhrase, double firstResult, String secondPhrase, double secondResult) {
+        System.out.printf(firstPhrase + firstResult + "\n" + secondPhrase + secondResult + "\n");
     }
+
+    private static void printResult(String phrase, double result) {
+        System.out.print(phrase + result);
+    }
+
 }
